a9b617fd181708113606c633b456d73b9d22dc5e
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-08-19 08:38:07 +0300

    full cashier done

--- cashier.py
+++ cashier.py
@@ -1,5 +1,20 @@
 def main():
-	# write code here
-
+	chosen = []
+	item = input ("Item (enter \"done\" when finished):")
+	while item !="done":
+		price = input("Price:")
+		quantity = input("Quantity:")
+		chosen.append({"name":item, "price": float(price), "quantity" : int(quantity)})
+		item = input ("Item (enter \"done\" when finished):")
+	print("-----------------")
+	print("receipt")
+	print("-----------------")
+	for item in chosen:
+		print(f"{item['quantity']} - {item['name']} - {item['quantity'] * item['price']}")
+	print("-----------------")
+	total = 0
+	for item in chosen:
+		total= total +item['quantity']*item['price']
+	print ("total: " + str(total) )
 if __name__ == '__main__':
 	main()
